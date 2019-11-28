c7a48b3c444756babc4adc1a05654f692838ee90
Herb vd Dool <herb@3speedhub.com>
2019-11-28 10:14:08 -0500

    fix icons in report

--- js/security_review.js
+++ js/security_review.js
@@ -5,15 +5,15 @@ Backdrop.behaviors.securityReview = function(context) {
     $.getJSON(anchor.attr('href') + '&js=1', function(data) {
       if (data.skip == true) {
         anchor.text('Enable');
-        row.attr('class' , 'info');
+        row.attr('class' , 'report-info');
       }
       else if (data.result == 1) {
         anchor.text('Skip');
-        row.attr('class' , 'ok');
+        row.attr('class' , 'report-status');
       }
       else {
         anchor.text('Skip');
-        row.attr('class' , 'error');
+        row.attr('class' , 'report-error');
       }
     });
     return false;
